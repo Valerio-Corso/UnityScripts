444324498aee09c471682b2e3b4e98c671cd245e
ValerioCorso <corso.valerio@gmail.com>
2019-02-04 19:03:08 +0100

    Added local save and player prefs helpers

---
+++ UnityScripts/Assets/Scripts/Helpers/LocalSaveController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class LocalSaveController : MonoBehaviour
+{
+    const string fileName = "/playerInfo.dat";
+
+    public int Coin { get; set; }
+
+    public int Record { get; set; }
+
+    public int SkinSelected { get; set; }
+
+    public bool HasFinishedTutorial { get; set; }
+
+    public bool IsFirstTimePlaying { get; set; }
+
+    public string FilePath { get; set; }
+
+    void Start()
+    {
+        FilePath = Application.persistentDataPath + fileName;
+
+        //check save presence
+        if (!File.Exists(FilePath))
+        {
+            SaveDefaultData();
+        }
+        //load data
+        LoadData();
+    }
+
+    /// <summary>
+    /// Set Default Initial data
+    /// </summary>
+    private void SaveDefaultData()
+    {
+        //open formatter and create file
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(FilePath);
+
+        PlayerData data = new PlayerData();
+        //get local variables
+        data.Coin = 0;
+        data.Record = 0;
+        data.SkinSelected = 0;
+
+        data.HasFinishedTutorial = false;
+        data.IsFirstTimePlaying = true;
+
+        //serialize and close
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    /// <summary>
+    /// Save All Data
+    /// </summary>
+    public void SaveAllData()
+    {
+        //open formatter and create file
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(FilePath);
+
+        PlayerData data = new PlayerData();
+        //get local variables
+        data.Coin = Coin;
+        data.IsFirstTimePlaying = IsFirstTimePlaying;
+        data.HasFinishedTutorial = HasFinishedTutorial;
+        data.Record = Record;
+        data.SkinSelected = SkinSelected;
+
+        //serialize and close
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    /// <summary>
+    /// Load data locally to avoid reading/writing from file each time
+    /// </summary>
+    public void LoadData()
+    {
+        if (File.Exists(FilePath))
+        {
+            //open formatter and file
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(FilePath, FileMode.Open);
+
+            //deserialize data
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            //assign values
+            Coin = data.Coin;
+            IsFirstTimePlaying = data.IsFirstTimePlaying;
+            HasFinishedTutorial = data.HasFinishedTutorial;
+            Record = data.Record;
+            SkinSelected = data.SkinSelected;
+        }
+    }
+
+    /// <summary>
+    /// The data written to a file
+    /// </summary>
+    [Serializable]
+    class PlayerData
+    {
+        public int Coin { get; set; }
+
+        public int SkinSelected { get; set; }
+
+        public int Record { get; set; }
+
+        public bool IsFirstTimePlaying { get; set; }
+
+        public bool HasFinishedTutorial { get; set; }
+    }
+}
+
+
---
+++ UnityScripts/Assets/Scripts/Helpers/PlayerPrefsController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+
+    #region Audio
+
+    const string volumeDialoguesKey = "dialoguesVolume";
+    const string volumeEffectsKey = "effectsVolume";
+    const string volumeMasterKey = "masterVolume";
+    const string volumeMusicKey = "musicVolume";
+
+    void StoreDialoguesVolume(float value)
+    {
+        PlayerPrefs.SetFloat(volumeDialoguesKey, value);
+    }
+
+    float GetDialoguesVolume(float defaultValue = 50)
+    {
+        return PlayerPrefs.GetFloat(volumeDialoguesKey, defaultValue);
+    }
+
+    void StoreEffectsVolume(float value)
+    {
+        PlayerPrefs.SetFloat(volumeEffectsKey, value);
+    }
+
+
+    float LoadEffectsVolume(float defaultValue = 50)
+    {
+        return PlayerPrefs.GetFloat(volumeEffectsKey, defaultValue);
+    }
+
+    void StoreMasterVolume(float value)
+    {
+        PlayerPrefs.SetFloat(volumeMasterKey, value);
+    }
+
+
+    float LoadMasterVolume(float defaultValue = 50)
+    {
+        return PlayerPrefs.GetFloat(volumeMasterKey, defaultValue);
+    }
+
+    void StoreMusicVolume(float value)
+    {
+        PlayerPrefs.SetFloat(volumeMusicKey, value);
+    }
+
+    float LoadMusicVolume(float defaultValue = 50)
+    {
+        return PlayerPrefs.GetFloat(volumeMusicKey, defaultValue);
+    }
+
+    #endregion
+
+    #region Language
+
+    const string languageKey = "language";
+
+    public enum Languages
+    {
+        English,
+        Italian,
+        German,
+        French
+    }
+
+    void StoreLanguage(Languages value)
+    {
+        PlayerPrefs.SetInt(languageKey, (int)value);
+    }
+
+    int LoadLanguage(int defaultValue = 0)
+    {
+        return PlayerPrefs.GetInt(languageKey, defaultValue);
+    }
+
+    #endregion
+
+    #region Internal Helpers
+
+    void SetBool(string key, bool value)
+    {
+        var intValue = value ? 1 : 0;
+        PlayerPrefs.SetInt(key, intValue);
+    }
+
+    bool GetBool(string key, bool defaultValue = false)
+    {
+        var value = PlayerPrefs.GetInt(key, defaultValue ? 1 : 0);
+
+        return value == 1;
+    }
+
+    #endregion
+}
